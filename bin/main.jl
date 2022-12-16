77101dc68c1d50481a43cdb904bb97f93a487339
Matt Bauman <mbauman@juliacomputing.com>
2022-12-16 16:10:02 -0500

    minimal HTML?

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,5 @@
 import Sockets, HTTP
 const ROUTER = HTTP.Router()
-sayhello(req) = HTTP.Response(200, "<h1>Hello World</h1>")
+sayhello(req) = HTTP.Response(200, "<html><body><h1>Hello World</h1></body></html>")
 HTTP.register!(ROUTER, "GET", "/", sayhello)
 HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", 8000)
