674e9046d81046769110f6eb03250f1421505cd1
Matt Bauman <mbauman@juliacomputing.com>
2022-12-16 15:27:52 -0500

    lowercase port?

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,6 @@
 import Sockets, HTTP
 const ROUTER = HTTP.Router()
 HTTP.register!(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World"))
-port = parse(Int, get(ENV, "PORT", "8000"))
+[@info("$k=$v") for (k,v) in ENV];
+port = parse(Int, get(ENV, "port", "8000"))
 HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", port)
