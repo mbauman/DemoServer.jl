6431514e635aba4e9b15d83d88c5c9e6f1587875
Matt Bauman <mbauman@juliahub.com>
2022-12-15 22:13:26 -0500

    fix router name

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,5 @@
 using Sockets, HTTP
-const router = HTTP.router()
-HTTP.@register(router, "GET", "/", req->HTTP.Response(200, "Hello World")))
+const ROUTER = HTTP.router()
+HTTP.@register(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World")))
 port = parse(Int, get(ENV, "PORT", "8000"))
-HTTP.serve(router, ip"0.0.0.0", port)
+HTTP.serve(ROUTER, ip"0.0.0.0", port)
