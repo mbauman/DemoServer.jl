aa6d1a0779304f266a16bc8f9c4aae1d79d20569
Matt Bauman <mbauman@juliacomputing.com>
2022-12-16 15:50:23 -0500

    show ARGS?

--- bin/main.jl
+++ bin/main.jl
@@ -1,6 +1,9 @@
 import Sockets, HTTP
 const ROUTER = HTTP.Router()
 HTTP.register!(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World"))
+@info "ENV"
 [@info("$k=$v") for (k,v) in ENV];
+@info "ARGS"
+[@info("$arg") for arg in ARGS];
 port = parse(Int, get(ENV, "port", "8000"))
 HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", port)
