29e7e694ccb97a0c7a2f8b3e8436c71268c2bf1b
Matt Bauman <mbauman@juliacomputing.com>
2022-12-16 16:05:32 -0500

    no port; simplify

--- bin/main.jl
+++ bin/main.jl
@@ -1,9 +1,5 @@
 import Sockets, HTTP
 const ROUTER = HTTP.Router()
-HTTP.register!(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World"))
-@info "ENV"
-[@info("$k=$v") for (k,v) in ENV];
-@info "ARGS"
-[@info("$arg") for arg in ARGS];
-port = parse(Int, get(ENV, "port", "8000"))
-HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", port)
+sayhello(req) = HTTP.Response(200, "<h1>Hello World</h1>")
+HTTP.register!(ROUTER, "GET", "/", sayhello)
+HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", 8000)
