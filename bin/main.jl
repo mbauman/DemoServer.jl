5f8b9a4dbf5023ef1b29ec5cd6a044fcc2d3a773
Matt Bauman <mbauman@juliahub.com>
2022-12-15 22:19:24 -0500

    Update main.jl

--- bin/main.jl
+++ bin/main.jl
@@ -1,5 +1,5 @@
 using Sockets, HTTP
 const ROUTER = HTTP.Router()
-HTTP.@register(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World")))
+HTTP.@register(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World"))
 port = parse(Int, get(ENV, "PORT", "8000"))
 HTTP.serve(ROUTER, ip"0.0.0.0", port)
