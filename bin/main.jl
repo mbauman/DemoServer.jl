46c7262ec1b0c9f319cad8b58ceb2409745a96ea
Matt Bauman <mbauman@juliacomputing.com>
2022-12-16 15:18:45 -0500

    use blocking serve()

--- bin/main.jl
+++ bin/main.jl
@@ -2,4 +2,4 @@ import Sockets, HTTP
 const ROUTER = HTTP.Router()
 HTTP.register!(ROUTER, "GET", "/", req->HTTP.Response(200, "Hello World"))
 port = parse(Int, get(ENV, "PORT", "8000"))
-HTTP.serve!(ROUTER, Sockets.ip"0.0.0.0", port)
+HTTP.serve(ROUTER, Sockets.ip"0.0.0.0", port)
